c150d00f66625fa61486fffbe1557cac3e4ed66e
shinpei_yoshihara <s-yoshihara@saitama-tec.co.jp>
2020-02-20 10:39:29 +0900

    the first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_sphere_mesh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
